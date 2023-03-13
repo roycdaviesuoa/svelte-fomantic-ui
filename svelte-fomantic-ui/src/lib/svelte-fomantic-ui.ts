ce2f6dd102519564f92c975291391394410c6b9e
Roy C. Davies <roy.davies@auckland.ac.nz>
2023-03-14 07:35:06 +1300

    Added some comments

--- svelte-fomantic-ui/src/lib/svelte-fomantic-ui.ts
+++ svelte-fomantic-ui/src/lib/svelte-fomantic-ui.ts
@@ -9,10 +9,16 @@
 export function uiProps (restProps:{}):string {
     let the_class = "";
     Object.keys(restProps).forEach((key, i) => {
+        // Properties that are boolean are used for the class details for Fomantic-UI.
+        // The class is only included if the property is true - this makes it possible to switch on and off a class programmatically.
+        // This works fine until you actually want to send a boolean data prop that you don't want to be used as a class name.
+        // So far, this hasn't been a problem...
         if ((typeof(restProps[key]) === "boolean") && restProps[key]) {
             the_class = the_class + ((i>0?" ":"") + key);
         }
     });
+
+    // Sometimes class details are sent in via the underscore property as text.
     if (restProps.hasOwnProperty("_"))
     {
         the_class = the_class + " " + restProps["_"]
@@ -31,20 +37,15 @@ export function otherProps (restProps:{}):{} {
     let other_props = {};
     Object.keys(restProps).forEach((key) => {
         if (key === "data") {
+            // Syntactic sugar for collecting data- properties into one JSON object.  Here we unwrap that back into data- properties.
             Object.keys(restProps[key]).forEach((data_key) => {
                 other_props["data-"+data_key] = restProps[key][data_key];
             })
         }
         else {
+            // Find all the non-boolean properties.
             if (typeof(restProps[key]) !== "boolean") {
-                if (typeof(restProps[key] === "string")) {
-                    if (restProps[key] !== "") {
-                        other_props[key] = restProps[key];
-                    }
-                }
-                else {
-                    other_props[key] = restProps[key];
-                }
+                other_props[key] = restProps[key];
             }
         }
     });
