fd6759ad5351399ee897c164da987ff0c12b3881
Roy C. Davies <roy.davies@auckland.ac.nz>
2023-04-20 09:22:26 +1200

    Still working on dropdown...

--- svelte-fomantic-ui/src/lib/modules/Dropdown.svelte
+++ svelte-fomantic-ui/src/lib/modules/Dropdown.svelte
@@ -14,18 +14,18 @@
     export let value: any = null;
     export let popup: object | boolean = undefined;
     export let multiple: boolean = false;
-    
+
         
     import { createEventDispatcher } from 'svelte';
     const dispatch = createEventDispatcher();
 
     // const multiple = $$restProps["multiple"];
 
-    function setSelected(e: any) {
+    function doChange(e: any) {
         console.log(e);
         selected = e.target.value;
 
-        if (e.target.attributes["data-value"]) {
+        if (e.target.attributes["data-value"]){
             value = e.target.attributes["data-value"].value;
             if (id) {
                 dispatch('change', {id: id, target: e.target, value: selected});
@@ -43,14 +43,36 @@
             }
         }
     }
+
+    // function doClick(e: any) {
+    //     console.log(e);
+
+    //     if (e.target.attributes["data-value"]) {
+    //         value = e.target.attributes["data-value"].value;
+    //         if (id) {
+    //             dispatch('click', {id: id, target: e.target, value: selected});
+    //         }
+    //         else {
+    //             dispatch('click', {target: e.target, value: selected});
+    //         }
+    //     }
+    //     else {
+    //         if (id) {
+    //             dispatch('click', {id: id, target: e.target});
+    //         }
+    //         else {
+    //             dispatch('click', {target: e.target});
+    //         }
+    //     }
+    // }
 </script>
 
 {#if multiple}
-    <div {id} class={classString(ui, $$restProps, "multiple dropdown")} data-selected={selected} data-module={rationalize([serialize("dropdown", {settings}), serialize((popup?"popup":null), (typeof(popup) === "boolean")?undefined:popup)])} {...otherProps($$restProps)} on:change={setSelected}>
+    <div {id} class={classString(ui, $$restProps, "multiple dropdown")} data-selected={selected} data-module={rationalize([serialize("dropdown", {settings}), serialize((popup?"popup":null), (typeof(popup) === "boolean")?undefined:popup)])} {...otherProps($$restProps)} on:change={doChange}>
         <slot />
     </div>
 {:else}
-    <div {id} class={classString(ui, $$restProps, "dropdown")} data-module={rationalize([serialize("dropdown", {settings}), serialize((popup?"popup":null), (typeof(popup) === "boolean")?undefined:popup)])} {...otherProps($$restProps)} on:change={setSelected}>
+    <div {id} class={classString(ui, $$restProps, "dropdown")} data-module={rationalize([serialize("dropdown", {settings}), serialize((popup?"popup":null), (typeof(popup) === "boolean")?undefined:popup)])} {...otherProps($$restProps)} on:change={doChange}>
         <slot />
     </div>
 {/if}
