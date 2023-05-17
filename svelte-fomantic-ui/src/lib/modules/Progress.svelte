ca918801483e83e230da58e1d66ea5dd567dcf9f
Roy Davies <roy.c.davies@ieee.org>
2023-05-18 07:23:01 +1200

    WIP contextual functions 2

--- svelte-fomantic-ui/src/lib/modules/Progress.svelte
+++ svelte-fomantic-ui/src/lib/modules/Progress.svelte
@@ -12,14 +12,28 @@
     export let settings: object = undefined;
     export let activate: boolean = false;
     export let popup: object | boolean = undefined;
-    export let functions : object = undefined;
+
+    let module_functions = settings?(settings.hasOwnProperty("callbacks")?settings["callbacks"]:{}):{};
+    let popup_functions = (typeof(popup) === "boolean")?{}:(popup?(popup.hasOwnProperty("callbacks")?popup["callbacks"]:{}):{});
 
     import { onDestroy } from "svelte";
-    const ID = initialise(id, functions);
-    onDestroy(() => { decommission(ID, id, functions); });
+    const Module_ID = initialise(id, module_functions);
+    const Popup_ID = initialise(id, popup_functions);
+    onDestroy(() => { 
+        decommission(Module_ID, id, module_functions); 
+        decommission(Popup_ID, id, popup_functions); 
+    });
 
 </script>
 
-<div {id} class={classString(ui, $$restProps, "progress")} data-module={rationalize([serialize("progress", {...functionize(ID, id, functions), ...settings}, activate), serialize((popup?"popup":null), (typeof(popup) === "boolean")?undefined:popup)])} {...otherProps($$restProps)}>
+<div {id} 
+    class={classString(ui, $$restProps, "progress")} 
+    data-module={
+        rationalize([
+            serialize("progress", {...functionize(Module_ID, id, module_functions), ...settings}, activate), 
+            serialize((popup?"popup":null), (typeof(popup) === "boolean")?undefined:{...functionize(Popup_ID, id, popup_functions), ...popup})
+        ])
+    } 
+    {...otherProps($$restProps)}>
     <slot />
 </div>
--- svelte-fomantic-ui/src/lib/tests/Test.svelte
+++ svelte-fomantic-ui/src/lib/tests/Test.svelte
@@ -5,13 +5,26 @@
 -->
 
 <script lang="ts">
-    import { Input, Icon, Popup, Content, Calendar } from "../svelte-fomantic-ui.svelte";
+    import { update, behavior, Progress, Segment, Button, Bar, Label, Divider } from "../svelte-fomantic-ui.svelte";
+
+    let example1_value: number = 0;
+    let parameter = null;
 </script>
 
-<Input ui fluid left icon popup={{position :"bottom right", hoverable: true}}>
-    <Icon calendar/>
-    <Input placeholder="Date/Time"/>
-</Input>
-<Popup ui settings={{position :"bottom right", hoverable: true}}>
-    <Content><Calendar ui></Calendar></Content>
-</Popup>
+<Segment ui>
+    <Button ui blue fluid on:click={()=>{update("example1"); example1_value = behavior("example1", "get percent")}}>Update</Button>
+    <Divider ui/>
+    <Progress ui teal data-percent={74} id="example1" popup={{content:"A Popup on a progress bar", position:"bottom center", callbacks:{
+        onChange : {
+            percent: parameter,
+            value: parameter,
+            total: parameter,
+
+            _: (data) => {console.log(data);}
+        }
+    }}}>
+        <Bar/>
+        <Label>{example1_value}% Funded</Label>
+    </Progress>
+</Segment>
+
