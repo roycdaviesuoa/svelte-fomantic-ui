509ef7f38dedefaade9bd508c920e2ec9ff567fe
Roy C. Davies <roy.davies@auckland.ac.nz>
2023-05-24 07:28:45 +1200

    Tests

--- svelte-fomantic-ui/src/lib/tests/Test.svelte
+++ svelte-fomantic-ui/src/lib/tests/Test.svelte
@@ -5,13 +5,37 @@
 -->
 
 <script lang="ts">
-    import { Input, Icon, Popup, Content, Calendar } from "../svelte-fomantic-ui.svelte";
+    import { update, behavior, parameter, Progress, Segment, Grid, Row, Column, Button, Bar, Label } from "../svelte-fomantic-ui.svelte";
+
+    const exampleData = {
+        total: 3, 
+        value: 0
+    };
+    const callBacks = {
+        onChange: {
+                percent: parameter,
+                value: parameter,
+                total: parameter,
+                
+                _:(data)=>{console.log(data);}
+            }
+        };
 </script>
 
-<Input ui fluid left icon popup={{position :"bottom right", hoverable: true}}>
-    <Icon calendar/>
-    <Input placeholder="Date/Time"/>
-</Input>
-<Popup ui settings={{position :"bottom right", hoverable: true}}>
-    <Content><Calendar ui></Calendar></Content>
-</Popup>
+<Segment ui>
+
+    <Grid ui>
+        <Row two column>
+            <Column><Button ui green fluid on:click={()=>{update("example4", exampleData)}}>Reset</Button></Column>
+            <Column><Button ui orange fluid on:click={()=>{behavior({id:"example4", type:'progress', commands:['increment']})}}>Increment</Button></Column>
+        </Row>
+    </Grid>
+    <Progress ui teal activate id="example4" settings={exampleData} functions={callBacks}>
+        <Bar>
+            <Progress />
+        </Bar>
+        <Label>Adding Photos</Label>
+    </Progress>
+
+</Segment>
+
