39c8939324c685a12c1b92f99e950df7cc9c0b8f
Roy Davies <roy.davies@auckland.ac.nz>
2023-06-01 12:43:10 +1200

    Form examples WIP

--- svelte-fomantic-ui/src/lib/tests/Form.svelte
+++ svelte-fomantic-ui/src/lib/tests/Form.svelte
@@ -1148,4 +1148,152 @@
     </Example>
     <!------------------------------------------------------------------------------------------------------------------------------------------------>
 
+
+
+    <!------------------------------------------------------------------------------------------------------------------------------------------------>
+    <!-- Validation rules -->
+    <!------------------------------------------------------------------------------------------------------------------------------------------------>
+    <Example title = "Validation rules" code = {Code.validation_rules}>
+
+        <Form ui segment settings={{
+            fields: {
+              name: {
+                identifier: 'name',
+                rules: [
+                  {
+                    type   : 'empty',
+                    prompt : 'Please enter your name'
+                  }
+                ]
+              },
+              skills: {
+                identifier: 'skills',
+                rules: [
+                  {
+                    type   : 'minCount[2]',
+                    prompt : 'Please select at least two skills'
+                  }
+                ]
+              },
+              gender: {
+                identifier: 'gender',
+                rules: [
+                  {
+                    type   : 'empty',
+                    prompt : 'Please select a gender'
+                  }
+                ]
+              },
+              username: {
+                identifier: 'username',
+                rules: [
+                  {
+                    type   : 'empty',
+                    prompt : 'Please enter a username'
+                  }
+                ]
+              },
+              password: {
+                identifier: 'password',
+                rules: [
+                  {
+                    type   : 'empty',
+                    prompt : 'Please enter a password'
+                  },
+                  {
+                    type   : 'minLength[6]',
+                    prompt : 'Your password must be at least {ruleValue} characters'
+                  }
+                ]
+              },
+              terms: {
+                identifier: 'terms',
+                rules: [
+                  {
+                    type   : 'checked',
+                    prompt : 'You must agree to the terms and conditions'
+                  }
+                ]
+              }
+            }
+          }}>
+            <p>Tell Us About Yourself</p>
+            <Fields two>
+                <Field>
+                    <Label input>Name</Label>
+                    <Input text placeholder="First Name" name="name"/>
+                </Field>
+                <Field>
+                    <Label>Gender</Label>
+                    <Select ui dropdown name="gender">
+                        <Option value="">Gender</Option>
+                        <Option value="male">Male</Option>
+                        <Option value="female">Female</Option>
+                    </Select>
+                </Field>
+            </Fields>
+            <Fields two>
+                <Field>
+                    <Label>Username</Label>
+                    <Input text placeholder="Username" name="username"/>
+                </Field>
+                <Field>
+                    <Label>Password</Label>
+                    <Input password name="password"/>
+                </Field>
+            </Fields>
+            <Field>
+                <Label>Skills</Label>
+                <Select ui dropdown name="skills" multiple>
+                    <Option value="">Select Skills</Option>
+                    <Option value="css">CSS</Option>
+                    <Option value="html">HTML</Option>
+                    <Option value="javascript">Javascript</Option>
+                    <Option value="design">Graphic Design</Option>
+                    <Option value="plumbing">Plumbing</Option>
+                    <Option value="mech">Mechanical Engineering</Option>
+                    <Option value="repair">Kitchen Repair</Option>
+                </Select>
+            </Field>
+            <Field inline>
+                <Checkbox ui>
+                    <Input checkbox name="terms"/>
+                    <Label input>I agree to the terms and conditions</Label>
+                </Checkbox>
+            </Field>
+            <Button ui primary submit>Submit</Button>
+            <Button ui error message></Button>
+        </Form>
+
+    </Example>
+    <!------------------------------------------------------------------------------------------------------------------------------------------------>
+
+
+
+    <!------------------------------------------------------------------------------------------------------------------------------------------------>
+    <!-- Validation passing parameters -->
+    <!------------------------------------------------------------------------------------------------------------------------------------------------>
+    <Example title = "Validation passing parameters" code = {Code.validation_passing_parameters}>
+
+        <Form ui segment settings={{
+            fields: {
+              color: {
+                identifier: 'color',
+                rules: [{
+                  type: 'regExp',
+                  value: /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/i,
+                }]
+              }
+            }
+          }}>
+            <Field>
+                <Label>Color</Label>
+                <Input text placeholder="Enter rgb" name="color" value="rgb(255, 255, 255)"/>
+            </Field>
+            <Button ui primary submit>Submit</Button>
+        </Form>
+
+    </Example>
+    <!------------------------------------------------------------------------------------------------------------------------------------------------>
+
 </Examples>
